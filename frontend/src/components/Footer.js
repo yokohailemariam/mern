61707cb6dfc3cb08e8c019d8ab9d34d8f2379118
yokohailemariam <33949690+yokohailemariam@users.noreply.github.com>
2021-06-08 18:31:00 +0300

    1

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,15 @@
+import { Col, Container, Row } from "react-bootstrap";
+
+const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col>Copyright &copy; Shop</Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">Shop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="#home">
+                <i className="fa fa-shopping-cart"></i> CART
+              </Nav.Link>
+              <Nav.Link href="#link">
+                <i className="fa fa-user"></i> SIGN IN
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
